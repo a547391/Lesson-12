0585fdc24466d3742fd76723bd3baa04de9b1532
joyce <a547391@gmail.com>
2025-08-11 14:35:36 +0800

    P15-19

--- drugstore.php
+++ drugstore.php
@@ -27,11 +27,42 @@
                     <?php require_once("hot.php"); ?>
                 </div>
                 <div class="col-md-10">
+                    <?php
+                    $level1Open = "";
+                    $level2Open = "";
+                    if (isset($_GET['level']) && isset($_GET['classid'])) {
+                        //選擇第一層類別
+                        $SQLstring = sprintf("SELECT * FROM pyclass WHERE level=%d AND classid=%d", $_GET['level'], $_GET['classid']);
+                        $classid_rs = $link->query($SQLstring);
+                        $data = $classid_rs->fetch();
+                        $level1Cname = $data['cname'];
+                        $level1Open = '<li class="breadcrumb-item active" aria-current="page">' . $level1Cname . '</li>';
+                    } elseif (isset($_GET['classid'])) {
+                        //選擇第二層類別
+                        $SQLstring = sprintf("SELECT * FROM pyclass where level=2 and classid=%d", $_GET['classid']);
+                        $classid_rs = $link->query($SQLstring);
+                        $data = $classid_rs->fetch();
+                        $level2Cname = $data['cname'];
+                        $level2Uplink = $data['uplink'];
+                        $level2Open = '<li class="breadcrumb-item active" aria-current="page">' . $level2Cname . '</li>'; //需加處理上一層
+
+                        $SQLstring = sprintf("SELECT * FROM pyclass where level=1 and classid=%d", $level2Uplink);
+                        $classid_rs = $link->query($SQLstring);
+                        $data = $classid_rs->fetch();
+                        $level1Cname = $data['cname'];
+                        $level1 = $data['level'];
+                        $level1Open = '<li class="breadcrumb-item"><a href="drugstore.php?classid=' . $level2Uplink . '&level=' . $level1 . '">' . $level1Cname . '</a></a></li>';
+                    }
+                    ?>
+
+
+
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#">首頁</a></li>
-                            <li class="breadcrumb-item"><a href="#">彩妝專區</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">隔離/防曬/飾底乳</li>
+                            <li class="breadcrumb-item"><a href="index.php">首頁</a></li>
+                            <?php echo $level1Open . $level2Open; ?>
+                            <!-- <li class="breadcrumb-item"><a href="#">彩妝專區</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">隔離/防曬/飾底乳</li> -->
                         </ol>
                     </nav>
 
