93ef6a98d43ebb94b65c00e1b98f65ba5072ec68
joyce <a547395@gmail.com>
2025-08-18 11:13:27 +0800

    P40-P48 新增goods.php 建立圖片產示品

--- drugstore.php
+++ drugstore.php
@@ -30,8 +30,29 @@
                     <!-- 建立類別分項-->
                     <?php require_once("breadcrumb.php");
                     ?>
-                    <!-- 引入 product藥粧商品-->
-                    <?php require_once("product_list.php"); ?>
+                    <!-- 產品詳細資訊-->
+                    <?php //require_once("goods_content.php"); 
+                    ?>
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-3">
+                                <img id="showGoods" name="showGoods" src="product_img/zoom2555551.webp" class="img-fluid rounded-start" alt="Biore 蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g">
+                                <div class="row mt-2">
+                                    <div class="col-md-4"><a href="product_img/zoom2555551.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
+                                    <div class="col-md-4"><a href="product_img/zoom2555552.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
+                                    <div class="col-md-4"><a href="product_img/zoom2555553.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
