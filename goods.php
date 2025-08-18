446932c8794dd24cfccc5d444489957f08fc4ccd
joyce <a547395@gmail.com>
2025-08-18 13:46:25 +0800

    P56-60

--- goods.php
+++ goods.php
@@ -45,16 +45,16 @@
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-<h5 class="card-title">Biore 蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g </h5>
-<p class="card-text">全新升級 礦物來源溫和潔淨配方讓你在香氛輕舞中實現淨亮柔嫩，柔和浪漫的櫻花香氛沉浸如春日裡瀰漫的愉悅舒心解放一天疲憊身心舒放無負擔</p>
-<h4 class="color_e600a0">$125</h4>
-<div class="row mt-3">
-    <div class="col-md-6">
-        <div class="input-group input-group-lg">
-            <span class="input-group-text color-success" id="inputGroup-sizing-lg">數量</span>
-            <input type="number" id="qty" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-        </div>
-    </div>
+                                    <h5 class="card-title">Biore 蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g </h5>
+                                    <p class="card-text">全新升級 礦物來源溫和潔淨配方讓你在香氛輕舞中實現淨亮柔嫩，柔和浪漫的櫻花香氛沉浸如春日裡瀰漫的愉悅舒心解放一天疲憊身心舒放無負擔</p>
+                                    <h4 class="color_e600a0">$125</h4>
+                                    <div class="row mt-3">
+                                        <div class="col-md-6">
+                                            <div class="input-group input-group-lg">
+                                                <span class="input-group-text color-success" id="inputGroup-sizing-lg">數量</span>
+                                                <input type="number" id="qty" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+                                            </div>
+                                        </div>
 
 
                                         <div class="col-md-6">
@@ -65,7 +65,7 @@
                             </div>
                         </div>
                     </div>
-
+                    <?php require_once('drop-box.php'); ?>
                 </div>
             </div>
         </div>
--- headfile.php
+++ headfile.php
@@ -6,5 +6,6 @@
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.1/css/all.css">
+ <link rel="stylesheet" href="ectable.css">
  <link rel="stylesheet" href="website_s01.css">
  <title>電商藥粧</title>
