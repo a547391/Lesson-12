6b931dd1fdf2fedf607532696984b5a87f405ace
joyce <a547395@gmail.com>
2025-08-18 14:22:14 +0800

    P67-68 建立產品資料顯示PHP顯示

--- goods.php
+++ goods.php
@@ -51,9 +51,9 @@
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Biore 蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g </h5>
-                                    <p class="card-text">全新升級 礦物來源溫和潔淨配方讓你在香氛輕舞中實現淨亮柔嫩，柔和浪漫的櫻花香氛沉浸如春日裡瀰漫的愉悅舒心解放一天疲憊身心舒放無負擔</p>
-                                    <h4 class="color_e600a0">$125</h4>
+                                    <h5 class="card-title"><?php echo $data['p_name']; ?></h5>
+                                    <p class="card-text"><?php echo $data['p_intro']; ?></p>
+                                    <h4 class="color_e600a0"><?php echo $data['p_price']; ?></h4>
                                     <div class="row mt-3">
                                         <div class="col-md-6">
                                             <div class="input-group input-group-lg">
@@ -71,7 +71,7 @@
                             </div>
                         </div>
                     </div>
-                    <?php require_once('drop-box.php'); ?>
+                    <?php echo $data['p_content']; ?>
                 </div>
             </div>
         </div>
