3c257c980da6555e41de3e15a10d8f8753ee9382
joyce <a547395@gmail.com>
2025-08-18 14:10:03 +0800

    P64-66 建立圖片顯示PHP程式

--- goods.php
+++ goods.php
@@ -35,12 +35,18 @@
                     ?>
                     <div class="card mb-3">
                         <div class="row g-0">
-                            <div class="col-md-3">
-                                <img id="showGoods" name="showGoods" src="product_img/zoom2555551.webp" class="img-fluid rounded-start" alt="Biore 蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g">
+                            <div class="col-md-4">
+                                <?php
+                                //取得產品圖片檔名資料
+                                $SQLstring = sprintf("SELECT * FROM product_img WHERE product_img.p_id=%d ORDER BY sort", $_GET['p_id']);
+                                $img_rs = $link->query($SQLstring);
+                                $imgList = $img_rs->fetch();
+                                ?>
+                                <img id="showGoods" name="showGoods" src="product_img/<?php echo $imgList['img_file']; ?>" class="img-fluid rounded-start" alt="<?php echo $data['p_name']; ?>" title="<?php echo $data['p_name']; ?>">
                                 <div class="row mt-2">
-                                    <div class="col-md-4"><a href="product_img/zoom2555551.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
-                                    <div class="col-md-4"><a href="product_img/zoom2555552.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
-                                    <div class="col-md-4"><a href="product_img/zoom2555553.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
+                                    <?php do { ?>
+                                        <div class="col-md-4"><a href="product_img/<?php echo $imgList['img_file']; ?>" rel="group" class="fancybox" title="<?php echo $data['p_name']; ?>"><img src="product_img/<?php echo $imgList['img_file']; ?>" class="img-fluid" alt="<?php echo $data['p_name']; ?>" title="<?php echo $data['p_name']; ?>"></a></div>
+                                    <?php } while ($imgList = $img_rs->fetch()); ?>
                                 </div>
                             </div>
                             <div class="col-md-8">
