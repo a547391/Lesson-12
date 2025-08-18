6b55d66c5a154bce8d750c75b79c233952631de8
joyce <a547395@gmail.com>
2025-08-18 11:45:55 +0800

    P49-55 建立產品說明區 新增CSS規則

--- goods.php
+++ goods.php
@@ -1,39 +1,86 @@
-<?php
-$level1Open = "";
-$level2Open = "";
-if (isset($_GET['search_name'])) {
-    //使用關鍵字查詢
-    $level1Open = '<li class="breadcrumb-item active" aria-current="page">關鍵字查詢：' . $_GET['search_name'] . '</li>';
-} elseif (isset($_GET['level']) && isset($_GET['classid'])) {
-    //選擇第一層類別
-    $SQLstring = sprintf("SELECT * FROM pyclass WHERE level=%d AND classid=%d", $_GET['level'], $_GET['classid']);
-    $classid_rs = $link->query($SQLstring);
-    $data = $classid_rs->fetch();
-    $level1Cname = $data['cname'];
-    $level1Open = '<li class="breadcrumb-item active" aria-current="page">' . $level1Cname . '</li>';
-} elseif (isset($_GET['classid'])) {
-    //選擇第二層類別
-    $SQLstring = sprintf("SELECT * FROM pyclass where level=2 and classid=%d", $_GET['classid']);
-    $classid_rs = $link->query($SQLstring);
-    $data = $classid_rs->fetch();
-    $level2Cname = $data['cname'];
-    $level2Uplink = $data['uplink'];
-    $level2Open = '<li class="breadcrumb-item active" aria-current="page">' . $level2Cname . '</li>'; //需加處理上一層
+<!-- 這是將資料庫，連線程式載入-->
+<?php require_once('Connections/dbset.php'); ?>
+<!-- 如果SESSION沒有啟動，則啟動SEEION功能，這是跨網頁變數存取  -->
+<?php (!isset($_SESSION)) ? session_start() : ""; ?>
+<!-- 載入共用PHP函數庫 -->
+<?php require_once("php_lib.php"); ?>
+<!doctype html>
+<html lang="zh-TW">
 
-    $SQLstring = sprintf("SELECT * FROM pyclass where level=1 and classid=%d", $level2Uplink);
-    $classid_rs = $link->query($SQLstring);
-    $data = $classid_rs->fetch();
-    $level1Cname = $data['cname'];
-    $level1 = $data['level'];
-    $level1Open = '<li class="breadcrumb-item"><a href="drugstore.php?classid=' . $level2Uplink . '&level=' . $level1 . '">' . $level1Cname . '</a></a></li>';
-}
-?>
+<head>
+    <!-- 引入網頁標頭 -->
+    <?php require_once("headfile.php"); ?>
+</head>
+
+<body>
+    <Section id="header">
+        <!-- 引入導覽列 -->
+        <?php require_once("navbar.php"); ?>
+    </Section>
+    <Section id="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2">
+                    <!-- 引入sidebar分類導覽 -->
+                    <?php require_once("sidebar.php"); ?>
+                    <!-- 引入熱銷商品模組 -->
+                    <?php require_once("hot.php"); ?>
+                </div>
+                <div class="col-md-10">
+                    <!-- 建立類別分項-->
+                    <?php require_once("breadcrumb.php");
+                    ?>
+                    <!-- 產品詳細資訊-->
+                    <?php //require_once("goods_content.php"); 
+                    ?>
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-3">
+                                <img id="showGoods" name="showGoods" src="product_img/zoom2555551.webp" class="img-fluid rounded-start" alt="Biore 蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g">
+                                <div class="row mt-2">
+                                    <div class="col-md-4"><a href="product_img/zoom2555551.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
+                                    <div class="col-md-4"><a href="product_img/zoom2555552.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
+                                    <div class="col-md-4"><a href="product_img/zoom2555553.webp"><img src="product_img/zoom2555551.webp" alt="蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g" class="img-fluid"></a></div>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+<h5 class="card-title">Biore 蜜妮淨嫩沐浴乳 浪漫櫻花香 水采保濕型 1000g </h5>
+<p class="card-text">全新升級 礦物來源溫和潔淨配方讓你在香氛輕舞中實現淨亮柔嫩，柔和浪漫的櫻花香氛沉浸如春日裡瀰漫的愉悅舒心解放一天疲憊身心舒放無負擔</p>
+<h4 class="color_e600a0">$125</h4>
+<div class="row mt-3">
+    <div class="col-md-6">
+        <div class="input-group input-group-lg">
+            <span class="input-group-text color-success" id="inputGroup-sizing-lg">數量</span>
+            <input type="number" id="qty" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+        </div>
+    </div>
 
 
+                                        <div class="col-md-6">
+                                            <button name="button01" id="button01" type="button" class="btn btn-success btn-lg color-success">加入購物車</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.php">首頁</a></li>
-        <?php echo $level1Open . $level2Open; ?>
-    </ol>
-</nav>
+                </div>
+            </div>
+        </div>
+    </Section>
+    <hr>
+    <Section id="scontent">
+        <!-- 服務說明-->
+        <?php require_once("scontent.php"); ?>
+    </Section>
+    <Section id="footer">
+        <!-- 聯絡資訊-->
+        <?php require_once("footer.php"); ?>
+    </Section>
+    <!-- 引入javascript檔-->
+    <?php require_once("jsfile.php"); ?>
+</body>
+
+</html>
