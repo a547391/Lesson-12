f7d03523662b2eb80d4fe7488f70660941cd7167
joyce <a547395@gmail.com>
2025-08-19 10:42:35 +0800

    P77-78 將goods.php功能加入到product.php裡面

--- goods.php
+++ goods.php
@@ -45,9 +45,9 @@
                                 ?>
                                 <img id="showGoods" name="showGoods" src="product_img/<?php echo $imgList['img_file']; ?>" class="img-fluid rounded-start" alt="<?php echo $data['p_name']; ?>" title="<?php echo $data['p_name']; ?>">
                                 <div class="row mt-2">
-<?php do { ?>
-<div class="col-md-4"><a href="product_img/<?php echo $imgList['img_file']; ?>" rel="group" class="fancybox" title="<?php echo $data['p_name']; ?>"><img src="product_img/<?php echo $imgList['img_file']; ?>" class="img-fluid" alt="<?php echo $data['p_name']; ?>" title="<?php echo $data['p_name']; ?>"></a></div>
-<?php } while ($imgList = $img_rs->fetch()); ?>
+                                    <?php do { ?>
+                                        <div class="col-md-4"><a href="product_img/<?php echo $imgList['img_file']; ?>" rel="group" class="fancybox" title="<?php echo $data['p_name']; ?>"><img src="product_img/<?php echo $imgList['img_file']; ?>" class="img-fluid" alt="<?php echo $data['p_name']; ?>" title="<?php echo $data['p_name']; ?>"></a></div>
+                                    <?php } while ($imgList = $img_rs->fetch()); ?>
                                 </div>
                             </div>
                             <div class="col-md-8">
