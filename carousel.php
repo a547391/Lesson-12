4a7bbf1582cb1eba57c12047c4de969c694d7a0b
joyce <a547391@gmail.com>
2025-08-25 09:03:21 +0800

    P79-81 將goods.php組合到carousel網頁

--- carousel.php
+++ carousel.php
@@ -16,7 +16,8 @@
             $i = 0;
             while ($data = $carousel->fetch()) { ?>
              <div class="carousel-item <?php echo activeShow($i, 0); ?>">
-                 <img src="./product_img/<?php echo $data['caro_pic']; ?>" class="d-block w-100" alt="<?php echo $data['caro_title']; ?>">
+                 <a href="goods.php?p_id=<?php echo $data['p_id']; ?>">
+                     <img src="./product_img/<?php echo $data['caro_pic']; ?>" class="d-block w-100" alt="<?php echo $data['caro_title']; ?>"></a>
                  <div class="carousel-caption d-none d-md-block">
                      <h5><?php echo $data['caro_title']; ?></h5>
                      <p><?php echo $data['caro_content']; ?></p>
